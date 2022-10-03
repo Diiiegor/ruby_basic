7ef9e9798fa9f2ef2da46e1bcfa098bccb0886eb
Juan Diego <jdoliveros@gtsolutions.io>
2022-10-02 22:22:51 -0500

    arrays examples

--- 04-arrays.rb
+++ 04-arrays.rb
@@ -1,29 +1,29 @@
-letras = ["a", "b", "c", "d", "e", "f"]
+letras = %w[a b c d e f]
 puts letras.size
 puts letras[0]
 puts letras[-1]
-puts letras.include? "z"
+puts letras.include? 'z'
 puts letras.first
 puts letras.last
 numbers = [1, 2, 3, 4, 5, 6, 7]
 
-puts "numbers.count { |number| number > 5 }"
+puts 'numbers.count { |number| number > 5 }'
 puts numbers.count { |number| number > 5 }
 
-puts "numbers.map { |number| number * 5 }"
+puts 'numbers.map { |number| number * 5 }'
 puts numbers.map { |number| number * 5 }
 
-puts "numbers.select { |number| number.odd? }"
+puts 'numbers.select { |number| number.odd? }'
 puts numbers.select { |number| number.odd? }
 
-puts "numbers.min"
+puts 'numbers.min'
 puts numbers.min
-puts "numbers.max"
+puts 'numbers.max'
 puts numbers.max
-puts "numbers.sum"
+puts 'numbers.sum'
 puts numbers.sum
 
-puts "letras.sort"
+puts 'letras.sort'
 puts letras.sort
 
 # puts letras.class
