9778cba739466ad95873a2a61fc175a7b58d5a77
Juan Diego <jdoliveros@gtsolutions.io>
2022-10-02 22:21:17 -0500

    arrays examples

---
+++ 04-arrays.rb
@@ -0,0 +1,30 @@
+letras = ["a", "b", "c", "d", "e", "f"]
+puts letras.size
+puts letras[0]
+puts letras[-1]
+puts letras.include? "z"
+puts letras.first
+puts letras.last
+numbers = [1, 2, 3, 4, 5, 6, 7]
+
+puts "numbers.count { |number| number > 5 }"
+puts numbers.count { |number| number > 5 }
+
+puts "numbers.map { |number| number * 5 }"
+puts numbers.map { |number| number * 5 }
+
+puts "numbers.select { |number| number.odd? }"
+puts numbers.select { |number| number.odd? }
+
+puts "numbers.min"
+puts numbers.min
+puts "numbers.max"
+puts numbers.max
+puts "numbers.sum"
+puts numbers.sum
+
+puts "letras.sort"
+puts letras.sort
+
+# puts letras.class
+# puts letras.methods
